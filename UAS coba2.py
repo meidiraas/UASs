1fc9e3d83c061011a36f1a95551e005783849426
meidiraas <meidiraas@gmail.com>
2024-07-25 21:26:30 +0700

    Add files via upload

---
+++ UAS coba2.py
@@ -0,0 +1,9 @@
+try:
+    class Queue:
+        def __init__(self,n=30)
+            self.size = n
+            self.current_size = 0
+            self.data = []
+
+        def isFull(self)
+            
